776e1ace42b99e79d4561d9cf6a5fe6ec2b274d0
Tizthebeast <164633466+Tizthebeast@users.noreply.github.com>
2025-11-12 12:27:46 -0800

    Experimental

--- Server.js
+++ Server.js
@@ -1,34 +1,37 @@
-require('dotenv').config()
-let mysql = require('mysql2');
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
 
-let connector = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
-})
+app.use(express.json());
 
-connector.connect(function(err){
-    if(err) throw err;
-    console.log("Connected!")
+// DB connection
+const connector = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-function SignUP(){
-    let UsernameDecoy = document.getElementById("Username");
-    let PasswordDecoy = document.getElementById("Password");
+connector.connect(err => {
+    if (err) throw err;
+    console.log("Connected to DB!");
+});
+
+// Signup endpoint
+app.post('/signup', (req, res) => {
+    const { Username, Password } = req.body;
     connector.query(
-        "SELECT * FROM authentication.info",(err,results) => {
-            if(err) throw err;
-            console.log(results);
-        }); 
-    connector.query(
-        "INSERT INTO info (Username,Password) values(?,?)",[UsernameDecoy,PasswordDecoy],
-        (err,result) => {
-            if(err){
+        "INSERT INTO info (Username, Password) VALUES (?, ?)",
+        [Username, Password],
+        (err, result) => {
+            if (err) {
                 console.error(err);
                 return res.status(500).send("Error inserting user");
             }
             res.send("User signed up successfully!");
         }
-    )}
-    
+    );
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
