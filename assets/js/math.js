b1afa299cbb274a081b5ff9b8835391b40960902
Tizthebeast <164633466+Tizthebeast@users.noreply.github.com>
2025-11-12 07:14:58 -0800

    nul

---
+++ assets/js/math.js
@@ -0,0 +1,11 @@
+function add(){
+    let number1 = document.getElementById('input_1').value;
+    let number2 = document.getElementById('input_2').value;
+    document.getElementById('decoy').innerHTML= number1+number2;
+}
+
+function Multiply(){
+    let number1 = document.getElementById('input_1').value;
+    let number2 = document.getElementById('input_2').value;
+    document.getElementById('decoy').innerHTML= number1*number2;
+}
